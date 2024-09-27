ec84f828da8365e37111498d2a1c58fa876b8c2f
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-09-27 20:27:45 +0800

    connected server to mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,25 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import mongoose from "mongoose"
 
 const app = new Hono()
 
+// Middleware
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+   return c.text('Server is running.')
 })
+
+const connectWithRetry = () => {
+   mongoose.connect(process.env.MONGODB_URI!)
+      .then(() => console.log("MongoDB Connected."))
+      .catch(err => {
+         console.log("MongoDB Connection Error: ", err);
+         console.log("Retrying in 0.5s second...");
+         setTimeout(connectWithRetry, 500);
+      });
+};
+
+connectWithRetry();
 
 export default app
