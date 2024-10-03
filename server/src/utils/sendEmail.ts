cc6ebacc1e3a1f48cd89ca4a393aac32603405e3
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-10-03 14:57:18 +0800

    update: removed console log

--- server/src/utils/sendEmail.ts
+++ server/src/utils/sendEmail.ts
@@ -4,8 +4,8 @@ export const sendEmail = async (email: string, code: string) => {
    const transporter = nodemailer.createTransport({
       service: 'Gmail',
       auth: {
-         user: process.env.GMAIL_USER, // Your Gmail address
-         pass: process.env.GMAIL_PASS, // Your Gmail app password
+         user: process.env.GMAIL_USER,
+         pass: process.env.GMAIL_PASS,
       },
    });
 
@@ -16,9 +16,7 @@ export const sendEmail = async (email: string, code: string) => {
          subject: 'Your Verification Code',
          text: `Your verification code is: ${code}. It will expire in 5 minutes.`,
       });
-      console.log(`Email sent to ${email}`);
    } catch (error) {
-      console.error(`Error sending email to ${email}: `, error);
       throw new Error('Email could not be sent.');
    }
 };
