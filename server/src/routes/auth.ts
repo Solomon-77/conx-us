60f9bfa93bdc0cd3118978398f34c423a59a62f8
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-10-04 10:30:33 +0800

    update: added login route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { signUp } from "../controllers/auth.signup";
 import { verify } from "../controllers/auth.verify";
+import { login } from "../controllers/auth.login";
 
 const router = new Hono();
 
 router.post("/signup", signUp)
 router.post("/verify", verify)
+router.post("/login", login)
 
 export default router;
