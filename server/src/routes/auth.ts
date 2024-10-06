5f12af9ae16550c3f1a0dc5b17e910165f3a7e12
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-10-06 20:10:37 +0800

    update: added google oauth route endpoint

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -8,5 +8,7 @@ const router = new Hono();
 router.post("/signup", signUp)
 router.post("/verify", verify)
 router.post("/login", login)
+router.get('/auth/google',);
+router.get('/auth/google/callback',);
 
 export default router;
