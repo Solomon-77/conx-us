5397170dc15173b24c440d9844172d7626a9b978
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-10-09 14:42:46 +0800

    update: connected functions to routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,14 +2,15 @@ import { Hono } from "hono";
 import { signUp } from "../controllers/auth.signup";
 import { verify } from "../controllers/auth.verify";
 import { login } from "../controllers/auth.login";
+import { googleOauth } from "../controllers/auth.oauth";
 
 const router = new Hono();
 
 router.post("/signup", signUp)
 router.post("/verify", verify)
 router.post("/login", login)
-router.get('/auth/google',);
-router.get('/auth/google/callback',);
+router.get('/auth/google', googleOauth.initiateGoogleAuth);
+router.get('/auth/google/callback', googleOauth.handleGoogleCallback);
 router.get('/auth/github',);
 router.get('/auth/github/callback',);
 
