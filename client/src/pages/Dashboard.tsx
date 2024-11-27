0d269c1498c0c775e20001d69f6bf80c5a15419f
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-11-27 21:46:35 +0800

    cursor

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -68,7 +68,7 @@ export default function Dashboard() {
                         </div>
                         <h1>bookmark</h1>
                      </div>
-                  </div>
+                  </div>z
                </div>
                <div className="">
                   <h1 className="text-lg font-bold mb-4">Trending</h1>
@@ -77,21 +77,21 @@ export default function Dashboard() {
                         <li className="font-bold">#iShowSpeed</li>
                         <li className="text-sm text-gray-500 font-medium mb-2">60.5k posts</li>
                      </div>
-                     <IoIosMore className="text-md" />
+                     <IoIosMore className="text-md cursor-pointer" />
                   </ul>
                   <ul className="flex justify-between items-center">
                      <div>
                         <li className="font-bold">#Iran</li>
                         <li className="text-sm text-gray-500 font-medium mb-2">40.5k posts</li>
                      </div>
-                     <IoIosMore className="text-md" />
+                     <IoIosMore className="text-md cursor-pointer" />
                   </ul>
                   <ul className="flex justify-between items-center">
                      <div>
                         <li className="font-bold">#Avalanche</li>
                         <li className="text-sm text-gray-500 font-medium mb-2">32.4k posts</li>
                      </div>
-                     <IoIosMore className="text-md" />
+                     <IoIosMore className="text-md cursor-pointer" />
                   </ul>
                </div>
             </div>
