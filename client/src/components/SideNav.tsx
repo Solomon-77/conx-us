49b9a9b3461d68dcbcc99f359438a0387bd262c7
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-12-01 20:57:15 +0800

    ui update

--- client/src/components/SideNav.tsx
+++ client/src/components/SideNav.tsx
@@ -7,35 +7,37 @@ import { FaBagShopping } from "react-icons/fa6";
 import { FaRankingStar } from "react-icons/fa6";
 
 export default function SideNav() {
+   const nav = "flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg cursor-pointer"
+
    return (
       <div className="">
          <ul className="space-y-3">
-            <li className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg">
+            <li className={nav}>
                <GoHomeFill className="text-2xl mr-3" />
                <h1 className="font-medium">Home</h1>
             </li>
-            <li className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg">
+            <li className={nav}>
                <FaPeopleGroup className="text-2xl mr-3" />
                <h1 className="font-medium">Guild</h1>
             </li>
-            <li className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg">
+            <li className={nav}>
                <FaRankingStar className="text-2xl mr-3" />
                <h1 className="font-medium">Rank</h1>
             </li>
-            <li className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg">
+            <li className={nav}>
                <IoBookmark className="text-2xl mr-3" />
                <h1 className="font-medium">Bookmarks</h1>
             </li>
-            <li className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg">
+            <li className={nav}>
                <AiFillQuestionCircle className="text-2xl mr-3" />
                <h1 className="font-medium">Ask</h1>
             </li>
-            <li className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg">
+            <li className={nav}>
                <FaBagShopping className="text-2xl mr-3" />
                <h1 className="font-medium">Shop</h1>
             </li>
             <li className="border-b border-gray-200" />
-            <li className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-lg">
+            <li className={nav}>
                <IoMdSettings className="text-2xl mr-3" />
                <h1 className="font-medium">Settings</h1>
             </li>
